0be6643f198ad8851c0ee526bd5c0708e7011d21
mario491 <sitoem05@gmail.com>
2022-10-18 16:11:12 -0700

    4 -6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - return the abslute value of nr
+ * @nr: target int nr
+ * Return: the %
+ */
+
+int _abs(int nr)
+{
+	if (t >= 0)
+		return (nr);
+	else
+		return (-nr);
